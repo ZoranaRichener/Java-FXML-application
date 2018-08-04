1cc514c416747b1dd627529078d66e18b442e4d3
Zorana <ladyzorzy@gmail.com>
2018-08-04 18:39:13 +0200

    Tab Galerija commit

--- TehnickiPregled/src/fxmlcontroller/GalerijaFXMLController.java
+++ TehnickiPregled/src/fxmlcontroller/GalerijaFXMLController.java
@@ -5,9 +5,25 @@
  */
 package fxmlcontroller;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
+import javax.imageio.ImageIO;
 
 /**
  * FXML Controller class
@@ -15,7 +31,74 @@ import javafx.fxml.Initializable;
  * @author Lenovo
  */
 public class GalerijaFXMLController implements Initializable {
+    
+       @FXML
+    private ScrollPane scrollPane;
 
+    @FXML
+    private AnchorPane imageSlider;
+
+    @FXML
+    private AnchorPane imagePane;
+
+    @FXML
+    private StackPane imgStackPane;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private Button btnUpload;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    void hendleDeleteBtn(ActionEvent event) {
+         
+    }
+
+    @FXML
+    void hendleSaveBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void hendleUploadBtn(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+              
+            //Set extension filter
+            FileChooser.ExtensionFilter extFilterJPG = 
+                    new FileChooser.ExtensionFilter("JPG files (*.JPG)", "*.JPG");
+            FileChooser.ExtensionFilter extFilterjpg = 
+                    new FileChooser.ExtensionFilter("jpg files (*.jpg)", "*.jpg");
+            FileChooser.ExtensionFilter extFilterPNG = 
+                    new FileChooser.ExtensionFilter("PNG files (*.PNG)", "*.PNG");
+            FileChooser.ExtensionFilter extFilterpng = 
+                    new FileChooser.ExtensionFilter("png files (*.png)", "*.png");
+            fileChooser.getExtensionFilters()
+                    .addAll(extFilterJPG, extFilterjpg, extFilterPNG, extFilterpng);
+ 
+            //Show open file dialog
+            File file = fileChooser.showOpenDialog(null);
+             
+            try {
+                BufferedImage bufferedImage = ImageIO.read(file);
+                Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+                imageView.setImage(image);
+                imageView.setPreserveRatio(true);
+               
+            } catch (IOException ex) {
+                Logger.getLogger(GalerijaFXMLController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+       
+    
+       
+    
     /**
      * Initializes the controller class.
      */
