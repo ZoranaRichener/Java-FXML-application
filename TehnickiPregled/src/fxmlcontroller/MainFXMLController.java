84cf3dbc03b97df334076b5faf596a404f925290
Zorana <ladyzorzy@gmail.com>
2018-08-04 13:01:57 +0200

    Main controller commit

--- TehnickiPregled/src/fxmlcontroller/MainFXMLController.java
+++ TehnickiPregled/src/fxmlcontroller/MainFXMLController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tehnickipregled;
+package fxmlcontroller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,6 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
 
 /**
  *
@@ -18,15 +19,18 @@ import javafx.scene.control.Label;
  */
 public class MainFXMLController implements Initializable {
     
+   
+   @FXML
+    private Tab tabKlijent;
+
     @FXML
-    private Label label;
-    
+    private Tab tabVozila;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private Tab tabTehnicki;
+
+    @FXML
+    private Tab tabGalerija;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
