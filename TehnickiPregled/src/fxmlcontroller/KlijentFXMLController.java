dbfedc19b6f2b813506c825e7887104a80b4339e
Vladimir Simic <simic0711@gmail.com>
2018-08-06 01:37:24 +0200

    Klijent
    upis klijenta u bazu

--- TehnickiPregled/src/fxmlcontroller/KlijentFXMLController.java
+++ TehnickiPregled/src/fxmlcontroller/KlijentFXMLController.java
@@ -5,10 +5,18 @@
  */
 package fxmlcontroller;
 
+import DAOKlase.KlijentDAO;
+import entiteti.Klijent;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 
@@ -18,8 +26,8 @@ import javafx.scene.layout.HBox;
  * @author Lenovo
  */
 public class KlijentFXMLController implements Initializable {
-    
-     @FXML
+
+    @FXML
     private TextField txtIme;
 
     @FXML
@@ -35,17 +43,73 @@ public class KlijentFXMLController implements Initializable {
     private TextField txtAdresa;
 
     @FXML
-    private HBox txtOpstina;
+    private TextField txtOpstina;
 
     @FXML
     private TextField txtBrojMobilnog;
+
+    @FXML
+    private Button btnSave;
+
+    private final KlijentDAO klijentDAO = new KlijentDAO();
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
+        btnSave.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+                
+
+                    Klijent noviKlijent = new Klijent(getID());
+
+                    noviKlijent.setIme(txtIme.getText());
+
+                    noviKlijent.setPrezime(txtPrezime.getText());
+
+                    noviKlijent.setJmbg(txtJMBG.getText());
+
+                    noviKlijent.setBrojLicneKarte(txtBrojLicneKarte.getText());
+
+                    noviKlijent.setAdresa(txtAdresa.getText());
+
+                    noviKlijent.setOpstina(txtOpstina.getText());
+
+                    noviKlijent.setBrojMobilnog(txtBrojMobilnog.getText());
+
+                    try {
+                        klijentDAO.addKlijent(noviKlijent);
+                    } catch (Exception ex) {
+                        Logger.getLogger(TehnickiPregledFXMLController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                
+            }
+
+        });
+
+    }
+
+    private int getID() {
+
+        List<Klijent> allKlijent = klijentDAO.getAllKlijent();
+
+        int maxID = 0;
+
+        if (allKlijent.isEmpty()) {
+
+            for (Klijent klijent : allKlijent) {
+                if (klijent.getKlijentiId() > maxID) {
+                    maxID = klijent.getKlijentiId();
+                }
+            }
+        }
+
+        return ++maxID;
+    }
+
 }
