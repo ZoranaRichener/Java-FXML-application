62b1ee4b55f06f6de02cc1d8916a37083ee3eb09
Vladimir Simic <simic0711@gmail.com>
2018-08-06 13:04:21 +0200

    Vozilo
    upis vozila u bazu

--- TehnickiPregled/src/fxmlcontroller/VoziloFXMLController.java
+++ TehnickiPregled/src/fxmlcontroller/VoziloFXMLController.java
@@ -5,21 +5,32 @@
  */
 package fxmlcontroller;
 
+import DAOKlase.VoziloDAO;
+import entiteti.Vozilo;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+
 /**
  * FXML Controller class
  *
  * @author Lenovo
  */
 public class VoziloFXMLController implements Initializable {
-    
-     @FXML
+
+    @FXML
     private TextField txtMarkaVozila;
 
     @FXML
@@ -97,13 +108,103 @@ public class VoziloFXMLController implements Initializable {
     @FXML
     private Button btnPronadjiVoziloAMSkatalog;
 
+    private final VoziloDAO voziloDAO = new VoziloDAO();
 
-    /**
-     * Initializes the controller class.
-     */
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
+        btnSnimiVozilo.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+                Vozilo noviVozilo = new Vozilo(getID());
+
+                noviVozilo.setMarkaVozila(txtMarkaVozila.getText());
+
+                noviVozilo.setModleVozila(tztModelVozila.getText());
+
+                noviVozilo.setTipVozila(txtTipVozila.getText());
+
+                noviVozilo.setBrojSasije(txtBrojSasije.getText());
+
+                noviVozilo.setBrojMotora(txtBrojMotora.getText());
+
+                noviVozilo.setSnagaMotora(txtSnagaMotora.getText());
+
+                noviVozilo.setZapremainaMotora(txtZapreminaMotora.getText());
+
+                noviVozilo.setGodinaProizvodnje(txtGodinaProizvodnje.getText());
+
+                noviVozilo.setZemljaProizvodnje(txtZemljaProizvodnje.getText());
+
+                noviVozilo.setDozvoljenaNosivost(txtDozvoljenaNosivost.getText());
+
+                noviVozilo.setMasaPraznogVozila(txtMasaPraznogVozila.getText());
+
+                noviVozilo.setNajvecaDozvoljenaMasa(txtNajvecaDozvoljenaMasa.getText());
+
+                noviVozilo.setSifraPoslednjegTehnickog(txtSifraPoslednjegTehnickog.getText());
+
+                noviVozilo.setBrojMestaUVozilu(txtBrojMestaZaSedenje.getText());
+
+                noviVozilo.setStajanje(txtBrojMestaStajanje.getText());
+
+                noviVozilo.setBojaVozila(txtBojaVozila.getText());
+
+                noviVozilo.setVrstaBoja(txtVrstaBoje.getText());
+
+                noviVozilo.setVrstaGoriva(txtVrstaGoriva.getText());
+
+                noviVozilo.setPredjeniPut(txtPredjeniPut.getText());
+
+                noviVozilo.setKuka(txtKuka.getText());
+
+                noviVozilo.setBrojOsovina(txtBrojOsovina.getText());
+
+                noviVozilo.setOsnovnaNamena(txtOsnovnaNamena.getText());
+
+                LocalDate localDate = datePDatumPoslenjegTehnickog.getValue();
+
+                Date date1 = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+                noviVozilo.setDatumPoslednjegTehnickog(date1);
+
+                LocalDate localDate2 = datePDatumIstekaSaobracajne.getValue();
+
+                Date date2 = Date.from(localDate2.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+                noviVozilo.setDatumIstekaSaobracajne(date2);
+
+                try {
+                    voziloDAO.addVozilo(noviVozilo);
+                } catch (Exception ex) {
+                    Logger.getLogger(TehnickiPregledFXMLController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            }
+
+        });
+
+    }
+
+    private int getID() {
+
+        List<Vozilo> allVozilo = voziloDAO.getAllVozilo();
+
+        int maxID = 0;
+
+        if (allVozilo.isEmpty()) {
+
+            for (Vozilo vozilo : allVozilo) {
+                if (vozilo.getVoziloId() > maxID) {
+                    maxID = vozilo.getVoziloId();
+                }
+            }
+        }
+
+        return ++maxID;
+    }
+
 }
